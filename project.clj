60074399a19e8bef067660cc0c70f61dc468a83b
Kyle Dawkins <kdawkins@fastmail.fm>
2014-09-27 22:59:32 -0700

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject node-cljs/lein-template "0.1.2"
+(defproject node-cljs/lein-template "0.1.3"
   :description "Leiningen template for Node.js-hosted ClojureScript"
   :url "https://github.com/quile/node-cljs-template"
   :license {:name "Eclipse Public License"
